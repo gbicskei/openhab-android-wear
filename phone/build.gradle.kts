26391de03e630e0996fed48ac8d464742fc60ec0
Gabor Bicskei <gabor.bicskei@loxon.eu>
2026-08-06 21:15:00 +0200

    release: 1.1.0 (watch=38, phone=39)

--- phone/build.gradle.kts
+++ phone/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "org.openhab.habdroid.wear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 37
+        versionCode = 39
         versionName = "1.1.0"
     }
 
